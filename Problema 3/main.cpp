66d95864a9a160b69d44ffeca6069172519a1eb4
cezXrar24 <cezar.bailescu@genesis.ro>
2026-03-10 08:05:39 +0000

    Problema 3

---
+++ Problema 3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n, x, cc;
+    cout << "Introdu n: ", cin >> n;
+    x = 1;
+    while(x <= n)
+    {
+        if (x == 0) cc = 0;
+          else 
+          {
+          cc = x % 9;
+          if (cc == 0)
+          {
+            cc = 9;
+          }
+          }
+        if (x % cc == 0)
+        {
+            cout << x << " ";
+        }
+        x++;
+    }
+    cout << cc << endl;
+    return 0;
+}
