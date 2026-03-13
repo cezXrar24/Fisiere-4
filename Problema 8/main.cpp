2e253175fb99e2f61e3d97d21227f99c1b5f3fea
cezXrar24 <cezar.bailescu@genesis.ro>
2026-03-13 09:05:43 +0000

    Fixed bug Problema 8

--- Problema 8/main.cpp
+++ Problema 8/main.cpp
@@ -4,24 +4,27 @@ using namespace std;
 
 int main() {
     int x, y, k;
-    cout << "Introdu x, ", cin >> x;
-    cout << "Introdu y, ", cin >> y;
-    cout << "Introdu k, ", cin >> k;
+    cout << "Introdu x: ";
+    cin >> x;
+    cout << "Introdu y: ";
+    cin >> y;
+    cout << "Introdu k: ";
+    cin >> k;
     int powx = 0;
     int powy = 0;
-    while(x > 0 && y > 0)
+    while(x > 0 || y > 0)
     {
         if(x % k == 0)
         {
-            powx++;
             x /= k;
+            powx++;
         }
         else
             x = 0;
         if(y % k == 0)
         {
-            powy++;
             y /= k;
+            powy++;
         }
         else
             y = 0;
@@ -30,9 +33,13 @@ int main() {
     {
         cout << "Cea mai mare putere a lui k este: " << powx << endl;
     }
-    else
+    else if (powy > powx)
     {
         cout << "Cea mai mare putere a lui k este: " << powy << endl;
+    }
+    else
+    {
+        cout << "Cea mai mare putere a lui k este: " << powx << endl;
     }
     return 0;
 }
